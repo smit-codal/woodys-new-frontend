c4e306074ecccb6a2f5bdc0412048e3ddb68a5b4
sraghani <sraghani@codal.com>
2023-08-29 20:02:54 +0530

    add font css stylesheet in layout file

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,6 +16,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="stylesheet" href="https://use.typekit.net/nvy8hpw.css" />
+      </head>
       <body>{children}</body>
     </html>
   );
